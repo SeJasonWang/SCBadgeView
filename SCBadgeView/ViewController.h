6489bdbf046d6b67e35b1d327b9a9f7631f7192e
sichenwang <52870104@qq.com>
2016-03-25 23:02:40 +0800

    Initial Commit

---
+++ SCBadgeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCBadgeView
+//
+//  Created by sichenwang on 16/3/25.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
