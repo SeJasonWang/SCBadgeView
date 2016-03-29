0ebf4968ec9821aeb97d5fdee0d634efc1e159cd
sichenwang <52870104@qq.com>
2016-03-29 23:22:59 +0800

    init

--- SCBadgeView/SCBadgeView/SCBadgeView.m
+++ SCBadgeView/SCBadgeView/SCBadgeView.m
@@ -10,49 +10,99 @@
 
 @implementation SCBadgeView
 {
-    UIImageView *_imageView;
-    UILabel *_textLabel;
+    UIButton *_button;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        self.backgroundColor = [UIColor clearColor];
-        self.hidden = YES;
-        _imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"SCBadgeView.bundle/badge_round_bg"]];
-        [self addSubview:_imageView];
-        _textLabel = [[UILabel alloc] init];
-        _textLabel.textAlignment = NSTextAlignmentCenter;
-        _textLabel.textColor = [UIColor whiteColor];
-        _textLabel.font = [UIFont systemFontOfSize:10];
-        _textLabel.adjustsFontSizeToFitWidth = YES;
-        _textLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
-        [self addSubview:_textLabel];
+        [self initializeSubViews];
     }
     return self;
 }
 
-- (void)layoutSubviews {
-    [super layoutSubviews];
-    
-    _imageView.frame = self.bounds;
-    _textLabel.frame = self.bounds;
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self initializeSubViews];
+    }
+    return self;
+}
+
+- (void)initializeSubViews {
+    self.hidden = YES;
+    self.backgroundColor = [UIColor clearColor];
+    _button = [UIButton buttonWithType:UIButtonTypeCustom];
+    _button.adjustsImageWhenHighlighted = NO;
+    _button.titleLabel.font = [UIFont systemFontOfSize:10];
+    [_button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self addSubview:_button];
+}
+
+- (void)setFrame:(CGRect)frame {
+    [super setFrame:CGRectMake(frame.origin.x, frame.origin.y, _button.frame.size.width, _button.frame.size.height)];
+}
+
+- (void)setBackgroundImageWithFrame:(CGRect)frame {
+    if (_button.currentImage.size.height != frame.size.height) {
+        UIImage *image = [self backgroundImageWithColor:[UIColor colorWithRed:255 / 255.0 green:102 / 255.0 blue:102 / 255.0 alpha:1] size:CGSizeMake(frame.size.height, frame.size.height)];
+        CGFloat width = image.size.width;
+        image = [image resizableImageWithCapInsets:UIEdgeInsetsMake(0, width / 2 - 0.5, 0, width / 2 - 0.5) resizingMode:UIImageResizingModeStretch];
+        [_button setBackgroundImage:image forState:UIControlStateNormal];
+    }
 }
 
 - (void)setNumber:(NSInteger)number {
     if (_number != number) {
         _number = number;
         
+        NSString *text;
         if (number > 0 && number < 100) {
-            _textLabel.text = [NSString stringWithFormat:@"%zd", number];
+            text = [NSString stringWithFormat:@"%zd", number];
             self.hidden = NO;
         } else if (number >= 100) {
-            _textLabel.text = @"99+";
+            text = @"99+";
             self.hidden = NO;
         } else {
-            _textLabel.text = @"";
+            text = @"";
             self.hidden = YES;
         }
+        [_button setTitle:text forState:UIControlStateNormal];
+        [_button.titleLabel sizeToFit];
+        CGRect frame = _button.titleLabel.frame;
+        if (_button.titleLabel.bounds.size.height >= _button.titleLabel.bounds.size.width) {
+            frame.size.height = _button.titleLabel.bounds.size.height + 5;
+            frame.size.width = frame.size.height;
+            frame.origin.x -= 2.5;
+            frame.origin.y -= 2.5;
+        } else {
+            frame.size.height = _button.titleLabel.bounds.size.height + 5;
+            frame.size.width = _button.titleLabel.bounds.size.width + 9;
+            frame.origin.x -= 4.5;
+            frame.origin.y -= 2.5;
+        }
+        [self setBackgroundImageWithFrame:frame];
+        _button.frame = frame;
+        self.bounds = frame;
     }
+}
+
+- (UIImage *)backgroundImageWithColor:(UIColor *)color size:(CGSize)size {
+    UIGraphicsBeginImageContext(size);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(ctx, [color CGColor]);
+    CGContextFillRect(ctx, CGRectMake(0, 0, size.width, size.height));
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIGraphicsBeginImageContextWithOptions(image.size, NO , 0);
+    ctx = UIGraphicsGetCurrentContext();
+    CGContextAddEllipseInRect(ctx, CGRectMake(0, 0, image.size.width, image.size.height));
+    CGContextClip(ctx);
+    CGContextStrokePath(ctx);
+    [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+    UIImage *circleImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return circleImage;
 }
 
 @end
--- SCBadgeView/ViewController.m
+++ SCBadgeView/ViewController.m
@@ -7,17 +7,42 @@
 //
 
 #import "ViewController.h"
-
+#import "SCBadgeView.h"
+#import "Masonry.h"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet SCBadgeView *badgeXib;
 
 @end
 
 @implementation ViewController
+{
+    SCBadgeView *_badge;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _badge = [[SCBadgeView alloc] init];
+    [self.view addSubview:_badge];
+    [_badge mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        _badge.number = 99;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.badgeXib.number = 12;
+    });
 }
+
+//- (void)viewDidLayoutSubviews {
+//    [super viewDidLayoutSubviews];
+//    
+//    _badge.frame = CGRectMake(100, 100, 30, 30);
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
