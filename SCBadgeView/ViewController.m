91ec6f71b357c47bc67c802849b42e82b29c318e
sichenwang <52870104@qq.com>
2016-05-23 15:04:25 +0800

    支持多类型

--- SCBadgeView/ViewController.m
+++ SCBadgeView/ViewController.m
@@ -21,15 +21,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    SCBadgeView *badgeLeft = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 100, 0, 0) alignment:SCBadgeViewAlignmentLeft];
+    SCBadgeView *badgeLeft = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 100, 0, 0) alignment:SCBadgeViewAlignmentLeft type:SCBadgeViewTypeDefault];
     badgeLeft.number = 12;
     [self.view addSubview:badgeLeft];
     
-    SCBadgeView *badgeCenter = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 130, 0, 0) alignment:SCBadgeViewAlignmentCenter];
+    SCBadgeView *badgeCenter = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 130, 0, 0) alignment:SCBadgeViewAlignmentCenter type:SCBadgeViewTypeWhiteBorder];
     badgeCenter.number = 23;
     [self.view addSubview:badgeCenter];
 
-    SCBadgeView *badgeRight = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 160, 0, 0) alignment:SCBadgeViewAlignmentRight];
+    SCBadgeView *badgeRight = [[SCBadgeView alloc] initWithFrame:CGRectMake(100, 160, 0, 0) alignment:SCBadgeViewAlignmentRight type:SCBadgeViewTypeWhiteBorder];
     badgeRight.number = 55;
     [self.view addSubview:badgeRight];
 //    [_badge mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -44,7 +44,7 @@
     });
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.badgeXib.number = 112;
+        self.badgeXib.number = 123;
     });
 }
 
